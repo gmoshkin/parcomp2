44f3b77d5ab7da43f4ec967b7473ce5777dd89f4
mgn <louielouie314@gmail.com>
2017-02-05 00:10:39 +0300

    Update test data to text converter

--- src/data2csv/entry.cpp
+++ src/data2csv/entry.cpp
@@ -19,7 +19,7 @@ using std::stringstream;
 using namespace Batcher;
 
 const char *usage =
-"Usage: data2csv in.data out.csv\n"
+"Usage: data2csv in.data [out.csv]\n"
 "\n"
 "in case n2 != 0:\n"
 "   in.data      input binary file\n"
@@ -27,12 +27,16 @@ const char *usage =
 
 int parseArguments(int argc, char *argv[], string &input, string &output)
 {
-    if (argc < 3) {
+    if (argc < 2) {
         cerr << usage;
         return -1;
     }
     input = argv[1];
-    output = argv[2];
+    if (argc > 2) {
+        output = argv[2];
+    } else {
+        output = "";
+    }
     return 0;
 }
 
@@ -53,16 +57,23 @@ void readData(const string &filename, points_t &data)
 
 void writeData(const string &filename, const points_t &data)
 {
-    ofstream out(filename.c_str(), ifstream::out);
-    out << setprecision(10);
+    if (filename.empty()) {
+        cout << setprecision(10);
+        for(points_it it = data.begin(); it != data.end(); it++) {
+            cout << it->getIndex() << ',' << it->getX() << ',' << it->getY() << endl;
+        }
+    } else {
+        ofstream out(filename.c_str(), ifstream::out);
+        out << setprecision(10);
 
-    // There's got to be a better way of doing this
-    size_t count = 0;
-    for(points_it it = data.begin(); it != data.end(); it++) {
-        out << it->getIndex() << ',' << it->getX() << ',' << it->getY() << endl;
-        count++;
+        // There's got to be a better way of doing this
+        size_t count = 0;
+        for(points_it it = data.begin(); it != data.end(); it++) {
+            out << it->getIndex() << ',' << it->getX() << ',' << it->getY() << endl;
+            count++;
+        }
+        cout << "Wrote " << count << " records to " << filename << endl;
     }
-    cout << "Wrote " << count << " records to " << filename << endl;
 }
 
 int main(int argc, char *argv[])
